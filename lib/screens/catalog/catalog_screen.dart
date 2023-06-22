c8bd5cde0eb09e27ed9bf32babaaa30598cb314e
Alena Grechka <alyonagrechka1@gmail.com>
2023-06-22 11:23:43 +0100

    step 13

--- lib/screens/catalog/catalog_screen.dart
+++ lib/screens/catalog/catalog_screen.dart
@@ -1,6 +1,8 @@
 import 'package:ecommerce_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../blocs/product/product_bloc.dart';
 import '../../models/category_model.dart';
 import '../../models/product_model.dart';
 
@@ -22,31 +24,46 @@ class CatalogScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<Product> categoryProducts = Product.products
-        .where((prd) => (prd.category == category.name))
-        .toList();
     return Scaffold(
       appBar: CustomAppBar(
         title: category.name,
       ),
       bottomNavigationBar: const CustomNavBar(screen: routeName),
-      body: GridView.builder(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 8.0,
-            vertical: 16.0,
-          ),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 1.15,
-          ),
-          itemCount: categoryProducts.length,
-          itemBuilder: (context, index) {
-            return Center(
-              child: ProductCard.catalog(
-                product: categoryProducts[index],
+      body: BlocBuilder<ProductBloc, ProductState>(
+        builder: (context, state) {
+          if (state is ProductLoading) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.black,
               ),
             );
-          }),
+          }
+          if (state is ProductLoaded) {
+            final List<Product> categoryProducts = state.products
+                .where((prd) => (prd.category == category.name))
+                .toList();
+            return GridView.builder(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                  vertical: 16.0,
+                ),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1.15,
+                ),
+                itemCount: categoryProducts.length,
+                itemBuilder: (context, index) {
+                  return Center(
+                    child: ProductCard.catalog(
+                      product: categoryProducts[index],
+                    ),
+                  );
+                });
+          } else {
+            return const Text('Something went wrong');
+          }
+        },
+      ),
     );
   }
 }
